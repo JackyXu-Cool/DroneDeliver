fc7b61b6c30c4dda62a1a88d34dc5bacc3717b8a
jxu477 <jxu477@gatech.edu>
2021-04-22 00:24:14 -0400

    update readme content, test procedure call

--- Readme.md
+++ Readme.md
@@ -7,15 +7,32 @@ The repository for Phase 4
 <br/>
 
 ### Backend setup instructions
-1. Change environment variables in nodemon.json.
-2. run 
+1. Change environment variables in **‘nodemon.json’**.
+2. MySQL database clean-up and setup
 ```
+打开 MySQL workbench
+
+// 这个SQL file是Phase3 TA提供给我们的
+运行 grocery_drone_delivery.sql
+
+// 这个SQL file是我们写的procedure
+运行 cs4400_phase3_shell.sql
+```
+3. run backend
+```
+cd node_backend
+
+// Install all the dependencies
+npm install
+
 npm start
 ```
-If not working:
 
+如果遇到bug的话，可以参考这个文章：
 https://stackoverflow.com/questions/52815608/er-not-supported-auth-mode-client-does-not-support-authentication-protocol-requ
+<br />
 
+如果需要初始化database，直接在workbench里，再把grocery_drone_delivery.mysql 跑一遍就好了。
 <br />
 
 ### Developers
--- node_backend/index.js
+++ node_backend/index.js
@@ -1,13 +1,59 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var con = mysql.createConnection({
+const app = express();
+
+// MySQL database connection setup
+const connection = mysql.createConnection({
     host: process.env.Mysql_host,
     user: process.env.Mysql_user,
     password: process.env.Mysql_password,
     database: process.env.Mysql_database
 });
-  
-con.connect(function(err) {
+
+
+connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Connected");
+    try {
+        app.listen(5000);
+        console.log("Listenning on Localhost:5000")
+    } catch(err) {
+        throw err;
+    }
 });
+
+// Rest API setup
+
+// Parse the request body from json to object format 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", 
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    next();
+});
+
+app.get("/test", async(req, res, next) => {
+    let sql = `CALL admin_create_grocery_chain(?)`;
+    connection.query(sql, "Hema food", (error) => {
+        if (error) {
+          next(error);
+        }
+        return res.json({"success": true});
+    });
+});
+
+app.use((req, res, next) => {
+    const error = new Error("Could not find the route", 404);
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.code || 500)
+    .json({message: error.message || "An unknown error occurs"})
+});
+
