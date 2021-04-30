50fa067fa03b2c052f5001a42103b9817bb6f095
jxu477 <jxu477@gatech.edu>
2021-04-30 14:33:44 -0400

    Add tracking progress on readme

---
+++ frontend/src/pages/CreateDronePage/CreateDronePage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+import classes from "./CreateDronePage.module.scss";
+
+const CreateDronePage = (props) => {
+    return (
+        <div className={classes.CreateDronePage}>  
+            <div className={classes.content}>
+                <h1 className={classes.title}>Admin Create Drone</h1>
+            
+                <div className={classes.buttons}>
+                    <button className={classes.back}>
+                        <Link to="/home" className={classes.link_back}>
+                            Back
+                        </Link>
+                    </button>
+                    <button className={classes.create}>
+                        Create
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CreateDronePage;
--- frontend/src/pages/CreateItemPage/CreateItemPage.js
+++ frontend/src/pages/CreateItemPage/CreateItemPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Dropdown from 'react-dropdown';
 import Entry from "../../components/Entry/Entry";
