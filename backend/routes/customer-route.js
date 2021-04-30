93a40eaa59739841b399d865ca09e0ef20208a1c
Jerry <6jerrylu@gmail.com>
2021-04-30 15:09:20 -0400

    Finish screen 14: Customer - View Order History

--- backend/routes/customer-route.js
+++ backend/routes/customer-route.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.get("/get/userfullname", customerController.get_customer_full_name);
 router.post("/change/ccinfo", customerController.change_credit_card_info);
+router.get("/get/orderIDs", customerController.get_order_ids_by_customer)
+router.get("/get/orderInfo", customerController.get_order_info)
 
 module.exports = router;
--- frontend/src/pages/ViewOrderHistoryPage/ViewOrderHistoryPage.js
+++ frontend/src/pages/ViewOrderHistoryPage/ViewOrderHistoryPage.js
@@ -4,11 +4,57 @@ import { Link } from "react-router-dom";
 import classes from "./ViewOrderHistoryPage.module.scss";
 
 const ViewOrderHistoryPage = (props) => {
+    let order_ids = props.orderIDs.map((entry) => {
+        return (<option key={entry} value={entry}>{entry}</option>);
+    })
+
     return (
         <div className={classes.ViewOrderHistoryPage}>
             <div className={classes.content}>
                 <h1 className={classes.title}>View Order History</h1>
+                <div className={classes.row}>
+                    <h2 className={classes.label}>Username: </h2>
+                    <input className={classes.entry} value={props.username} disabled/>
+                </div>
 
+                <div className={classes.row}>
+                    <h2 className={classes.label}>Order ID: </h2>
+                    <select className={classes.droplist} name="order_id" onChange={props.onSelect}>{order_ids}</select>
+                </div>
+
+
+                <div className={classes.row}>
+                    <h2 className={classes.label}>Total Amount: </h2>
+                    <input className={classes.entry} value={props.orderInfo.total_amount} disabled/>
+                </div>
+
+                <div className={classes.row}>
+                    <h2 className={classes.label}>Total Items: </h2>
+                    <input className={classes.entry} value={props.orderInfo.total_items} disabled/>
+                </div>
+
+                <div className={classes.row}>
+                    <h2 className={classes.label}>Date of Purchase: </h2>
+                    <input className={classes.entry} value={props.orderInfo.orderdate} disabled/>
+                </div>
+
+                <div className={classes.row}>
+                    <h2 className={classes.label}>Drone ID: </h2>
+                    <input className={classes.entry} value={props.orderInfo.droneID} disabled/>
+                </div>
+
+                <div className={classes.row}>
+                    <h2 className={classes.label}>Store Associate: </h2>
+                    <input className={classes.entry} value={props.orderInfo.dronetech} disabled/>
+                </div>
+
+                <div className={classes.row}>
+                    <h2 className={classes.label}>Status: </h2>
+                    <input className={classes.entry} value={props.orderInfo.orderstatus} disabled/>
+                </div>
+                <button className={classes.btn_back}>
+                    <Link className={classes.link_back} to="/home">Back</Link>
+                </button>
             </div>
         </div>
     );
